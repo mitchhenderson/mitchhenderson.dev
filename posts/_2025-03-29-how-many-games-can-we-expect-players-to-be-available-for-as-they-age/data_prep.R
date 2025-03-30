5399a5b282d1c08706fe7af4bbaa8a4ffb46d003
Mitch Henderson <mitch.henderson@hotmail.com>
2025-03-30 21:34:27 +1100

    Refine ZOIB model and dataset

--- posts/_2025-03-29-how-many-games-can-we-expect-players-to-be-available-for-as-they-age/data_prep.R
+++ posts/_2025-03-29-how-many-games-can-we-expect-players-to-be-available-for-as-they-age/data_prep.R
@@ -21,15 +21,53 @@ season_info <- match_information |>
 dates_of_birth <- read_csv("data/dates_of_birth_2014-2024_players.csv") |>
   mutate(dob = dmy(dob))
 
+# Create mapping tables to account for inconsistent names
+player_mapping <- player_list |>
+  summarize(
+    # Take the most frequent firstName for each playerId
+    firstName = names(sort(table(firstName), decreasing = TRUE)[1]),
+    # Take the most frequent lastName for each playerId
+    lastName = names(sort(table(lastName), decreasing = TRUE)[1]),
+    .by = playerId
+  )
 
-data <- player_list |>
+# Remove low involvement players
+players_with_less_than_100_games <- player_list |>
   drop_na(playerId) |>
   left_join(season_info, by = join_by(matchId)) |>
   mutate(teamId = if_else(team == "Home", homeTeamId, awayTeamId)) |>
   mutate(team_number_of_matches = n_distinct(matchId),
          .by = c(season, teamId)) |>
   summarise(matches_played = n_distinct(matchId),
-            .by = c(season, playerId, team_number_of_matches)) |>
+            .by = c(playerId)) |>
+  filter(matches_played < 100) |>
+  pull(playerId)
+
+# Remove player seasons who moved midyear
+players_who_moved_midseason <- player_list |>
+  drop_na(playerId) |>
+  left_join(season_info, by = join_by(matchId)) |>
+  mutate(teamId = if_else(team == "Home", homeTeamId, awayTeamId)) |>
+  mutate(team_number_of_matches = n_distinct(matchId),
+         .by = c(season, teamId)) |>
+  count(playerId, season, teamId) |>
+  count(playerId, season) |>
+  filter(n == 2)
+
+
+data <- player_list |>
+  drop_na(playerId) |>
+  filter(!playerId %in% players_with_less_than_100_games) |>
+  select(-firstName, -lastName) |> # Remove inconsistent names
+  left_join(player_mapping, by = join_by(playerId)) |> # Add the consistent ones
+  left_join(season_info, by = join_by(matchId)) |> # Add season
+  anti_join(players_who_moved_midseason, by = join_by(playerId, season)) |>
+  mutate(teamId = if_else(team == "Home", homeTeamId, awayTeamId),
+         fullName = glue::glue("{firstName} {lastName}")) |>
+  mutate(team_number_of_matches = n_distinct(matchId),
+         .by = c(season, teamId)) |>
+  summarise(matches_played = n_distinct(matchId),
+            .by = c(season, playerId, fullName, team_number_of_matches)) |>
   left_join(dates_of_birth, by = join_by(playerId == nrl_com_playerId)) |>
   mutate(reference_date = ymd(glue::glue("{season}-01-01")),
          age_at_start_of_year = interval(dob, reference_date) %/% years(1),
